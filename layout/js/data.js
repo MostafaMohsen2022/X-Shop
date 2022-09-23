64b9441447292df7ee63768c09a1e7a8c55c14d4
Mostafa Mohsen <107756172+MostafaMohsen2022@users.noreply.github.com>
2022-09-23 18:00:44 +0200

    x-shop

---
+++ layout/js/data.js
@@ -0,0 +1,40 @@
+let productsDB = [
+  {
+    id: 1,
+    title: "headphone",
+    desc: "We first reviewed the M50x back in 2014, so these over-ears are long established.",
+    size: "large",
+    imageUrl: "layout/images/t-shirt.webp",
+    qty: 1,
+    isMe: "N",
+  },
+  {
+    id: 2,
+    title: "Super combed supima cotton fabric",
+    desc: "100% Cotton, Regular Fit",
+    size: "small",
+    imageUrl: "layout/images/black-t-shirt.jpg",
+    qty: 1,
+    isMe: "N",
+  },
+  {
+    id: 3,
+    title: "T-Shirt for Men - Blue Coral",
+    desc: "Super combed supima cotton fabric",
+    size: "medium",
+    imageUrl: "layout/images/blue-t-shirt.webp",
+    qty: 1,
+    isMe: "N",
+  },
+  {
+    id: 4,
+    title: "3-pack T-shirts Slim fit",
+    desc: "Round-necked T-shirts in jersey made from a cotton-blend. Slim fit.",
+    size: "large",
+    imageUrl: "layout/images/3-pack.webp",
+    qty: 1,
+    isMe: "N",
+  },
+];
+
+// localStorage.setItem("products", JSON.stringify(productsDB));
